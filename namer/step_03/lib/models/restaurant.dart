4b34cddfb06a2fc4fffdd03b4b9ec638634feceb
tucole1-max <tucole1@gmail.com>
2025-11-23 19:21:33 -0500

    Add Restaurant model with properties and constructor

---
+++ namer/step_03/lib/models/restaurant.dart
@@ -0,0 +1,17 @@
+class Restaurant {
+  final String name;
+  final String cuisine;
+  final double distanceKm;
+  final double rating;
+  final int etaMinutes;
+  final String imageUrl;
+
+  Restaurant({
+    required this.name,
+    required this.cuisine,
+    required this.distanceKm,
+    required this.rating,
+    required this.etaMinutes,
+    required this.imageUrl,
+  });
+}
