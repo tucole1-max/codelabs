a2a193ee3abe6e1cf12a24d73bcdd9dfdb82d62c
tucole1-max <tucole1@gmail.com>
2025-11-23 19:23:56 -0500

    Create mock_restaurants.dart

---
+++ namer/step_03/lib/data/mock_restaurants.dart
@@ -0,0 +1,28 @@
+import '../models/restaurant.dart';
+
+final List<Restaurant> mockRestaurants = [
+  Restaurant(
+    name: "Sushi Palace",
+    cuisine: "Japanese",
+    distanceKm: 1.2,
+    rating: 4.7,
+    etaMinutes: 25,
+    imageUrl: "https://i.imgur.com/3q3Q7Qe.jpeg",
+  ),
+  Restaurant(
+    name: "Mama’s Italian",
+    cuisine: "Italian",
+    distanceKm: 0.8,
+    rating: 4.5,
+    etaMinutes: 20,
+    imageUrl: "https://i.imgur.com/FfvaU4o.jpeg",
+  ),
+  Restaurant(
+    name: "BBQ Brothers",
+    cuisine: "American",
+    distanceKm: 2.1,
+    rating: 4.2,
+    etaMinutes: 30,
+    imageUrl: "https://i.imgur.com/6t6P2Mm.jpeg",
+  ),
+];
